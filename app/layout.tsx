f866f017afd44749278c28ce6c9252354e5a1b79
kjw-blog <rkdwjddnr11@naver.com>
2023-10-30 22:42:25 +0900

    Feat) Clerk 한국어 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import { koKR } from '@clerk/localizations';
 
 const font = Open_Sans({ subsets: ['latin'] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={koKR}>
       <html lang="en">
         <body className={font.className}>{children}</body>
       </html>
