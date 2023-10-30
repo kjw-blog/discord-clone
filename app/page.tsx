332f6c0785b06f28d00a5ebd7701d63acf3ec203
kjw-blog <rkdwjddnr11@naver.com>
2023-10-30 21:47:05 +0900

    Feat) 버튼 UI 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
 export default function Home() {
-  return <p>Commit Test</p>;
+  return (
+    <div className="">
+      <p className="text-indigo-500 font-bold text-3xl">Hello Discord</p>
+      <Button>Click me</Button>
+    </div>
+  );
 }
