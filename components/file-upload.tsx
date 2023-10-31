2a0bf5dd3408074069601649190c017f1548d770
kjw-blog <rkdwjddnr11@naver.com>
2023-10-31 20:20:52 +0900

    Feat) 이미지 업로드 기능 추가

---
+++ components/file-upload.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { X } from 'lucide-react';
+import Image from 'next/image';
+import { UploadDropzone } from '@/lib/uploadthing';
+import '@uploadthing/react/styles.css';
+
+interface FileUploadProps {
+    onChange: (url?: string) => void;
+    value: string;
+    endpoint: 'messageFile' | 'serverImage';
+}
+
+export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+    const fileType = value?.split('.').pop();
+
+    if (value && fileType !== 'pdf') {
+        return (
+            <div className="relative h-20 w-20">
+                <Image
+                    fill
+                    src={value}
+                    alt="Upload"
+                    className="rounded-full bg-zinc-300"
+                />
+                <button
+                    onClick={() => onChange('')}
+                    className="absolute right-0 top-0 rounded-full bg-rose-500 p-1 text-white shadow-sm"
+                    type="button"
+                >
+                    <X className="h-4 w-4" />
+                </button>
+            </div>
+        );
+    }
+
+    return (
+        <UploadDropzone
+            content={{
+                label: '이미지를 선택하거나 드래그 해오세요.',
+                allowedContent: '이미지 (4MB)',
+                button: ({ ready }) => {
+                    return '파일 업로드';
+                },
+            }}
+            endpoint={endpoint}
+            onClientUploadComplete={(res) => {
+                onChange(res?.[0].url);
+            }}
+            onUploadError={(error: Error) => {
+                console.log(error);
+            }}
+        />
+    );
+};
--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,6 +1,6 @@
 'use client';
-import { useEffect, useState } from 'react';
 
+import { useEffect, useState } from 'react';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
@@ -24,6 +24,7 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import { FileUpload } from '@/components/file-upload';
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -79,7 +80,21 @@ const InitialModal = () => {
                     >
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                이미지 업로드할 위치
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload
+                                                    endpoint="serverImage"
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
                             </div>
                             <FormField
                                 control={form.control}
