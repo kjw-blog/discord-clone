6345cb96c4142933fcfed1fd42ec0c0eb408bf23
kjw-blog <rkdwjddnr11@naver.com>
2023-10-31 21:50:33 +0900

    Feat) navigation 작업중 - 서버 추가하기 버튼 추가 - 버튼 tooltip 컴포넌트 추가

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,3 +1,31 @@
-export const NavigationSidebar = () => {
-    return <div>Navigation Sidebar</div>;
+import { redirect } from 'next/navigation';
+
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { NavigationAction } from '@/components/navigation/navigation-action';
+import { Separator } from '@/components/ui/separator';
+
+export const NavigationSidebar = async () => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirect('/');
+    }
+
+    const servers = await db.server.findMany({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id,
+                },
+            },
+        },
+    });
+
+    return (
+        <div className="flex h-full w-full flex-col items-center space-y-4 py-3 text-primary dark:bg-[#1E1F22]">
+            <NavigationAction />
+            <Separator className="mx-auto h-[2px] w-10 rounded-md bg-zinc-300 dark:bg-zinc-700" />
+        </div>
+    );
 };
