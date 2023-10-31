740209b259cc047f182ac32df0e54a2f4e3de909
kjw-blog <rkdwjddnr11@naver.com>
2023-10-31 16:06:38 +0900

    Feat) 서버 만들기 모달창 추가 - 서버 만들기 모달창 추가 - react hook form + zod 적용

---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,118 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from '@/components/ui/dialog';
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+const formSchema = z.object({
+    name: z.string().min(1, {
+        message: '서버 이름이 필요합니다.',
+    }),
+    imageUrl: z.string().min(1, {
+        message: '서버 이미지가 필요합니다.',
+    }),
+});
+
+const InitialModal = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: '',
+            imageUrl: '',
+        },
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    };
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <Dialog open>
+            <DialogContent className="overflow-hidden bg-white p-0 text-black">
+                <DialogHeader className="px-6 pt-8">
+                    <DialogTitle className="text-center text-2xl font-bold">
+                        서버 만들기
+                    </DialogTitle>
+                    <DialogDescription className="text-center text-zinc-500">
+                        이름과 이미지로 서버 개인 정보를 지정합니다.
+                        <br />
+                        나중에 언제든지 변경할 수 있습니다.
+                    </DialogDescription>
+                </DialogHeader>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-8"
+                    >
+                        <div className="space-y-8 px-6">
+                            <div className="flex items-center justify-center text-center">
+                                이미지 업로드할 위치
+                            </div>
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-secondary/70">
+                                            서버 이름
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                autoComplete="off"
+                                                disabled={isLoading}
+                                                className="border-0 bg-zinc-300/50 text-black focus-visible:ring-0 focus-visible:ring-offset-0"
+                                                placeholder="서버명을 입력하세요"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <DialogFooter className="bg-gray-100 px-6 py-4">
+                            <Button variant="primary" disabled={isLoading}>
+                                만들기
+                            </Button>
+                        </DialogFooter>
+                    </form>
+                </Form>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default InitialModal;
