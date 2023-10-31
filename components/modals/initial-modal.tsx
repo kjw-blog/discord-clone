cab80c84288b2973abfc4dd68152476c2ef3d6fb
kjw-blog <rkdwjddnr11@naver.com>
2023-10-31 21:09:07 +0900

    Feat) create server api 추가

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 
 import {
@@ -38,6 +40,8 @@ const formSchema = z.object({
 const InitialModal = () => {
     const [isMounted, setIsMounted] = useState(false);
 
+    const router = useRouter();
+
     useEffect(() => {
         setIsMounted(true);
     }, []);
@@ -53,7 +57,16 @@ const InitialModal = () => {
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try {
+            await axios.post('/api/servers', values);
+
+            form.reset();
+
+            router.refresh();
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     if (!isMounted) {
