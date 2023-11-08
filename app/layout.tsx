65f694b80629cac9da2fe41a990f05b78171e5d9
kjw-blog <rkdwjddnr11@naver.com>
2023-11-08 17:16:43 +0900

    Feat: 채팅 개발
    - socket.io 세팅

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { koKR } from '@clerk/localizations';
 import { cn } from '@/lib/utils';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { ModalProvider } from '@/components/providers/modal-provider';
+import { SocketProvider } from '@/components/providers/socket-provider';
 
 const font = Open_Sans({ subsets: ['latin'] });
 
@@ -35,8 +36,10 @@ export default function RootLayout({
                         enableSystem={false}
                         storageKey="discord-theme"
                     >
-                        <ModalProvider />
-                        {children}
+                        <SocketProvider>
+                            <ModalProvider />
+                            {children}
+                        </SocketProvider>
                     </ThemeProvider>
                 </body>
             </html>
