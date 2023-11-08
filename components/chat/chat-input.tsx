0524a02f666a12b5421b772ec82fa80f9af7a84b
kjw-blog <rkdwjddnr11@naver.com>
2023-11-08 17:41:39 +0900

    Feat: 채팅 개발
    - 채팅 input  컴포넌트 추가

---
+++ components/chat/chat-input.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Plus, Smile } from 'lucide-react';
+
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+interface ChatInputProps {
+    apiUrl: string;
+    query: Record<string, any>;
+    name: string;
+    type: 'conversation' | 'channel';
+}
+
+const formSchema = z.object({
+    content: z.string().min(1),
+});
+
+export const ChatInput = ({ apiUrl, name, query, type }: ChatInputProps) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            content: '',
+        },
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (value: z.infer<typeof formSchema>) => {
+        console.log(value);
+    };
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+                <FormField
+                    control={form.control}
+                    name="content"
+                    render={(field) => (
+                        <FormItem>
+                            <FormControl>
+                                <div className="relative p-4 pb-6">
+                                    <button
+                                        type="button"
+                                        onClick={() => {}}
+                                        className="absolute left-8 top-7 flex h-[24px] w-[24px] items-center justify-center rounded-full bg-zinc-500 transition hover:bg-zinc-600 dark:bg-zinc-400 dark:hover:bg-zinc-300"
+                                    >
+                                        <Plus className="text-white dark:text-[#313338]" />
+                                    </button>
+                                    <Input
+                                        {...field}
+                                        disabled={isLoading}
+                                        className="border-0 border-none bg-zinc-200/90 px-14 py-6 text-zinc-600 focus-visible:ring-0 focus-visible:ring-offset-0 dark:bg-zinc-700/75 dark:text-zinc-200"
+                                        placeholder={`${
+                                            type === 'conversation'
+                                                ? '@' + name
+                                                : '#' + name
+                                        }에 메시지 보내기`}
+                                    />
+                                    <div className="absolute right-8 top-7">
+                                        <Smile />
+                                    </div>
+                                </div>
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+            </form>
+        </Form>
+    );
+};
