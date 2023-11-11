666ad432da1c1b8e4651d24cff70cc44589726c4
kjw-blog <rkdwjddnr11@naver.com>
2023-11-11 23:15:20 +0900

    feat: 채팅 개발
    - 메시지 삭제, 수정 UI 추가

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -1,11 +1,21 @@
 'use client';
 
+import * as z from 'zod';
+import qs from 'querystring';
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Member, MemberRole, Profile } from '@prisma/client';
-import { FileIcon, ShieldAlert, ShieldCheck } from 'lucide-react';
+import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from 'lucide-react';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 import { UserAvatar } from '@/components/user-avatar';
 import { ActionTooltip } from '@/components/action-tooltip';
+import { cn } from '@/lib/utils';
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 interface ChatItemProps {
     id: string;
@@ -28,6 +38,10 @@ const roleIconMap = {
     ADMIN: <ShieldAlert className="ml-2 h-4 w-4 text-rose-500" />,
 };
 
+const formSchema = z.object({
+    content: z.string().min(1),
+});
+
 export const ChatItem = ({
     content,
     currentMember,
@@ -40,6 +54,38 @@ export const ChatItem = ({
     socketUrl,
     timestamp,
 }: ChatItemProps) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    useEffect(() => {
+        const handleKeyDown = (event: any) => {
+            if (event.key === 'Escape' || event.keyCode === 27) {
+                setIsEditing(false);
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => window.removeEventListener('keydown', handleKeyDown);
+    }, []);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            content,
+        },
+    });
+
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    };
+
+    useEffect(() => {
+        form.reset({
+            content,
+        });
+    }, [content, form]);
+
     const fileType = fileUrl?.split('.').pop();
 
     const isAdmin = currentMember.role === MemberRole.ADMIN;
@@ -100,8 +146,72 @@ export const ChatItem = ({
                             </div>
                         </div>
                     )}
+                    {!fileUrl && !isEditing && (
+                        <p
+                            className={cn(
+                                'text-sm text-zinc-600 dark:text-zinc-300',
+                                deleted &&
+                                    'mt-1 text-xs italic text-zinc-500 dark:text-zinc-400',
+                            )}
+                        >
+                            {content}
+                            {isUpdated && !deleted && (
+                                <span className="mx-2 text-[10px] text-zinc-500 dark:text-zinc-400">
+                                    (수정됨)
+                                </span>
+                            )}
+                        </p>
+                    )}
+                    {!fileUrl && isEditing && (
+                        <Form {...form}>
+                            <form
+                                className="pt2 flex w-full items-center gap-x-2"
+                                onSubmit={form.handleSubmit(onSubmit)}
+                            >
+                                <FormField
+                                    control={form.control}
+                                    name="content"
+                                    render={({ field }) => (
+                                        <FormItem className="flex-1">
+                                            <FormControl>
+                                                <div className="relative w-full">
+                                                    <Input
+                                                        className="border-0 border-none bg-zinc-200/90 p-2 text-zinc-600 focus-visible:ring-0 focus-visible:ring-offset-0 dark:bg-zinc-700/75 dark:text-zinc-200"
+                                                        placeholder="메시지 수정하기"
+                                                        {...field}
+                                                    />
+                                                </div>
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button size="sm" variant="primary">
+                                    저장
+                                </Button>
+                            </form>
+                            <span className="mt-1 text-[10px] text-zinc-400">
+                                취소하려면 Esc키를, 저장하려면 Enter키를
+                                누르세요.
+                            </span>
+                        </Form>
+                    )}
                 </div>
             </div>
+            {canDeleteMessage && (
+                <div className="absolute -top-2 right-5 hidden items-center gap-x-2 rounded-sm border bg-white p-1 group-hover:flex dark:bg-zinc-800">
+                    {canEditMessage && (
+                        <ActionTooltip label="수정">
+                            <Edit
+                                onClick={() => setIsEditing(true)}
+                                className="ml-auto h-4 w-4 cursor-pointer text-zinc-500 transition hover:text-zinc-600 dark:hover:text-zinc-300"
+                            />
+                        </ActionTooltip>
+                    )}
+                    <ActionTooltip label="삭제">
+                        <Trash className="ml-auto h-4 w-4 cursor-pointer text-zinc-500 transition hover:text-zinc-600 dark:hover:text-zinc-300" />
+                    </ActionTooltip>
+                </div>
+            )}
         </div>
     );
 };
